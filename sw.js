dd023b2538e7968c5c7e31489b54562d7de04a58
JCWordPress <117703386+JCWordPress@users.noreply.github.com>
2022-11-24 09:29:21 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,67 @@
+// Code resource:
+// https://developers.google.com/web/fundamentals/primers/service-workers/
+
+const CACHE_NAME = 'cache-v1';
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps - open cache, cache files, confirm assets cached
+    event.waitUntil(
+        // takes a chain of promises
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        // pass in a promise
+        caches.match(event.request)
+        .then(function(response) {
+            // if matched, return cached value
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request).then(
+                function(response) {
+                    // Check if we received a valid response
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    // Clone the response 
+                    var responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                }
+            );
+        })
+    );
+});
